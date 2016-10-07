614964e412afe8298f43ab35259393ccb27114f5
truephredd <phredd@gmail.com>
2016-10-07 17:29:47 -0400

    good progress

--- plagmada_scrape.py
+++ plagmada_scrape.py
@@ -3,4 +3,6 @@ from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 plagmada = urlopen("http://plagmada.org/gallery/main.php")
 plagSoup = BeautifulSoup(plagmada.read(), "html.parser")
-print(plagSoup.h2)
+galleryList = plagSoup.findAll("td", {"class":"giAlbumCell gcBackground1"})
+for gallery in galleryList:
+	print(gallery.a)
