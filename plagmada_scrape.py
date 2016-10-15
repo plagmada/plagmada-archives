287c3929fd6ff182abe0288a0917c579f6703cc6
truephredd <phredd@gmail.com>
2016-10-14 20:59:18 -0400

    parsing albums tree

--- plagmada_scrape.py
+++ plagmada_scrape.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
+import os
 plagmada = urlopen("http://plagmada.org/gallery/main.php")
 plagSoup = BeautifulSoup(plagmada.read(), "html.parser")
 
@@ -13,5 +14,18 @@ galleryList = plagSoup.findAll("td", {"class":"giAlbumCell gcBackground1"})
 for gallery in galleryList:
 	album.append(gallery.a.get('href'))
 
+# Presently, this loads the first page of each album into Beautful Soup and prints out the H2 heading
+# H2 contains album names, so this is useful info.  Will likely use this to create directories for each album
+
 for i in range(0,len(album)):
-	print(album[i])
+	g2_itemId = album[i].split('&')
+	#print(g2_itemId[0])
+	album_page = urlopen ("http://plagmada.org/gallery/{0}".format(g2_itemId[0]))
+	alSoup = BeautifulSoup(album_page.read(), "html.parser")
+	# print(alSoup.h2)
+	album_name = str(alSoup.h2)
+	the_bits = album_name.split()
+	print("{}_{}".format(the_bits[1],the_bits[2]))
+
+# Just checking the functionality...
+# os.mkdir("test")
