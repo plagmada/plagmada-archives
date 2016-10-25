e961d72c7e140dd7bfbe96d0c67cc6fe51d2195d
truephredd <phredd@gmail.com>
2016-10-24 21:14:53 -0400

    Top Level Data Collection Progress

--- plagmada_scrape.py
+++ plagmada_scrape.py
@@ -2,30 +2,48 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 import os
-plagmada = urlopen("http://plagmada.org/gallery/main.php")
-plagSoup = BeautifulSoup(plagmada.read(), "html.parser")
+import re
+
+
+PlaGMaDA = urlopen("http://plagmada.org/gallery/main.php")
+mainSoup = BeautifulSoup(PlaGMaDA.read(), "html.parser")
 
 # galleryList
 # This seems to be the optimal way to collect gallery links.  It skips the link back to the introduction page and
 # since that's the only unwanted table cell, this is much easier than trying to play with the sibling function.
 
 album=[]
-galleryList = plagSoup.findAll("td", {"class":"giAlbumCell gcBackground1"})
-for gallery in galleryList:
-	album.append(gallery.a.get('href'))
+album_notes=[]
+album_size=[]
+galleryList = mainSoup.findAll("td", {"class":"giAlbumCell gcBackground1"})
+for galleryItem in galleryList:
+	album.append(galleryItem.a.get('href'))
+	album_notes.append(galleryItem.find('p', class_="giDescription"))
+	album_size.append(galleryItem.find('div', class_="size summary"))
 
-# Presently, this loads the first page of each album into Beautful Soup and prints out the H2 heading
-# H2 contains album names, so this is useful info.  Will likely use this to create directories for each album
+# This will iterate through each album page.  Ultimately, it will then iterate through each item page for the album.
+# Currently, it is grabbing the H2 header from the first page of the album and using regex to create a cromulent
+# directory name for each album.  
+#
+# g2_ItemID refers to the php tag that is used in page links.  g2_* is used throughout the Gallery2 markup.
+# I'll try to use the spefiic tag from there when appropriate.
+
 
 for i in range(0,len(album)):
 	g2_itemId = album[i].split('&')
-	#print(g2_itemId[0])
 	album_page = urlopen ("http://plagmada.org/gallery/{0}".format(g2_itemId[0]))
 	alSoup = BeautifulSoup(album_page.read(), "html.parser")
-	# print(alSoup.h2)
-	album_name = str(alSoup.h2)
-	the_bits = album_name.split()
-	print("{}_{}".format(the_bits[1],the_bits[2]))
+	
+	# Album Titles
+	album_title = alSoup.h2.get_text()
+	album_directory = re.sub(r' ',"_",album_title.strip())
+	album_directory = re.sub(r'\,',"",album_directory)
+	album_directory = re.sub(r'\"',"",album_directory)
+	print(album_directory)
+	if album_notes[i]:
+		print(album_notes[i].text.strip())
+	print(album_size[i])
+	print("---")
 
 # Just checking the functionality...
 # os.mkdir("test")
